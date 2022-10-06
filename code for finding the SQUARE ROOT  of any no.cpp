24d02f65d0507c3eb24c58d274fba4e11b4a0477
Harsh Rawat <112020551+Rawharsh@users.noreply.github.com>
2022-10-06 08:39:46 +0530

    Create code for finding the SQUARE ROOT  of any no.cpp
    CODE TO FIND THE SQUARE ROOT OF ANY NO. UP TO TWO FLOATING DIGIT USING C++.

---
+++ code for finding the SQUARE ROOT  of any no.cpp
@@ -0,0 +1,58 @@
+           CODE TO FIND THE SQUARE ROOT OF ANY NO.
+         
+         
+#include<iostream>
+using namespace std;
+
+ long long int sqrtInteger(int n) {       // FUNCTON FOR FINDING THE INTEGER PART OF THE SQUARE ROOT
+        
+        int s = 0;
+        int e = n;
+        long long int mid = s + (e-s)/2;
+        
+        long long int ans = -1;
+        while(s<=e) {
+            
+            long long int square = mid*mid;
+            
+            if(square == n)
+                return mid;
+            
+            if(square < n ){
+                ans = mid;
+                s = mid+1;
+            }
+            else
+            {
+                e = mid - 1;
+            }
+            mid = s + (e-s)/2;
+        }
+        return ans;
+    }
+
+double morePrecision(int n, int precision, int tempSol) {      // FUNCTION FOR FINDING THE FLOATING PART OF SQUARE ROOT
+    
+    double factor = 1;
+    double ans = tempSol;
+
+    for(int i=0; i<precision; i++) {
+        factor = factor/10;
+
+        for(double j=ans; j*j<n; j= j+factor ){
+            ans = j;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    int n;
+    cout <<" Enter the number " << endl;
+    cin >> n;
+
+    int tempSol = sqrtInteger(n);       // FUNTION CALLING
+    cout <<" Answer is " << morePrecision(n, 3, tempSol) << endl;    //FUNTION CALLING
+
+    return 0;
+}
