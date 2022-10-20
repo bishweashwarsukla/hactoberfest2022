918f1cf9c2818143b6eae66bc1adf979e8941018
amanaolsingh <115914853+amanaolsingh@users.noreply.github.com>
2022-10-20 11:47:35 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,16 @@
+
+public class Test 
+{ 
+	public static void main(String[] args) 
+	{ 
+		String s1 = "Ram"; 
+		String s2 = "Ram"; 
+		String s3 = "Shyam"; 
+		String s4 = "ABC"; 
+
+		System.out.println(" Comparing strings with compareTo:"); 
+		System.out.println(s1.compareTo(s2)); 
+		System.out.println(s1.compareTo(s3)); 
+		System.out.println(s1.compareTo(s4)); 
+	} 
+} 
