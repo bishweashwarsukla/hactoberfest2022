29d8e9fad3960dee0447ce9cdfc28dded9f76799
aksinghanp <73906564+aksinghanp@users.noreply.github.com>
2022-10-20 14:04:52 +0530

    Update Maximum Number of Robots Within Budget.cpp

--- Maximum Number of Robots Within Budget.cpp
+++ Maximum Number of Robots Within Budget.cpp
@@ -27,3 +27,27 @@ chargeTimes.length == runningCosts.length == n
 1 <= n <= 5 * 104
 1 <= chargeTimes[i], runningCosts[i] <= 105
 1 <= budget <= 1015
+
+ Answer:
+
+class Solution {
+public:
+    typedef long long ll;
+    int maximumRobots(vector<int>& ct, vector<int>& rc, long long budget) {
+        multiset<int> ms;
+        ll start=0,end=0,ans=0,n=ct.size(),s=0;
+        while(end<n){
+            s+=rc[end];
+            ms.insert(ct[end++]);
+            ll val = (*ms.rbegin())+(end-start)*s;
+            while(val>budget){
+                s-=rc[start];
+                ms.erase(ms.find(ct[start++]));
+                val =ms.empty()?0 : (*ms.rbegin())+(end-start)*s;
+            }
+            ans=max(ans,end-start);
+        }
+        return ans;
+    }
+};
+ 
